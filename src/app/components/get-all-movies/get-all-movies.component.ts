b556fd1ebea4226548c786dee7a02b86569f18f1
Parami <paramiashinsana@gmail.com>
2024-08-13 20:38:07 +0530

    add getAll package and service package

---
+++ src/app/components/get-all-movies/get-all-movies.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-get-all-movies',
+  standalone: true,
+  imports: [],
+  templateUrl: './get-all-movies.component.html',
+  styleUrl: './get-all-movies.component.css'
+})
+export class GetAllMoviesComponent {
+
+}
