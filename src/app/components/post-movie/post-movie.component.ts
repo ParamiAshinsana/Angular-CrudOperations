b79acb600824f31bec8f286ed283ec3ad4c553bf
Parami <paramiashinsana@gmail.com>
2024-08-13 20:34:55 +0530

    add update and post packages

---
+++ src/app/components/post-movie/post-movie.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-movie',
+  standalone: true,
+  imports: [],
+  templateUrl: './post-movie.component.html',
+  styleUrl: './post-movie.component.css'
+})
+export class PostMovieComponent {
+
+}
